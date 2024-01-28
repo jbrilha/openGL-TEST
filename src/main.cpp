c336e7d7dd74a2113f021cbff71d2a0e899af86a
jbrilha <j.brilha@campus.fct.unl.pt>
2024-01-28 16:53:00 +0000

    Added camera tracking to the blocks :D

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include <chrono>
 #include <cmath>
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
-#include <ratio>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <thread>
 
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/fwd.hpp"
@@ -20,17 +18,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
-// typedef struct Vertex {
-//     vec2 pos;
-//     vec3 col;
-// } Vertex;
-//
-// static const Vertex vertices[4] =
-//     {{{-0.9f, -0.9f}, {1.f, 0.f, 0.f}},
-//     {{0.9f, -0.9f}, {0.f, 1.f, 0.f}},
-//     {{0.9f, 0.9f}, {1.f, 1.f, 0.f}},
-//     {{-0.9f, 0.9f}, {0.f, 0.f, 1.f}}};
 
 static void process_input(GLFWwindow *window);
 static void error_callback(int error, const char *description);
@@ -53,11 +40,11 @@ int nb_frames = 0;
 int last_time = 0;
 
 // viewport settings
-int width = 600;
-int height = 500;
+int width = 800;
+int height = 800;
 
 // camera
-Camera camera(glm::vec3(0.f, 0.f, 3.f));
+Camera camera(glm::vec3(0.f, 3.f, 7.f));
 float last_x = width / 2.f;
 float last_y = height / 2.f;
 bool first_mouse = true;
@@ -67,6 +54,7 @@ bool paused = false;
 float delta_time = 0.f;
 float last_frame = 0.f;
 
+bool track = false;
 // glm::mat4 projection = glm::mat4(1.0f);
 
 int main(int argc, char *argv[]) {
@@ -74,8 +62,8 @@ int main(int argc, char *argv[]) {
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -115,68 +103,77 @@ int main(int argc, char *argv[]) {
 
     float cube[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.f,
+         0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.f,
+         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.f,
+         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.f,
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.f,
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 0.f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.f,
 
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.f,
+         0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.f,
+         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.f,
          0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.f,
          0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.f,
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.f,
+         0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.f,
          0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.f,
          0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.f,
+         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.f,
+         0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.f,
+         0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.f,
+    };
+    float floor[] = {
+          15.f, 0.f, -15.f, 0.5f, 0.5f, 0.5f,
+         -15.f, 0.f, -15.f, 0.5f, 0.5f, 0.5f, // right to left far
+          15.f, 0.f,  15.f, 0.5f, 0.5f, 0.5f,
+
+         -15.f, 0.f,  15.f, 0.f, 0.f, 0.f,
+         -15.f, 0.f, -15.f, 0.f, 0.f, 0.f,
+          15.f, 0.f,  15.f, 0.f, 0.f, 0.f,
     };
 
     glm::vec3 cubePositions[] = {
         glm::vec3( 0.0f,  0.0f,  0.0f),
         glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.5f, 2.2f, -2.5f),
+        glm::vec3(-3.8f, 2.0f, -12.3f),
+        glm::vec3( 2.4f, 3.f, -3.5f),
         glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.3f, 2.0f, -2.5f),
         glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3( 1.5f,  5.f, -1.5f),
         glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
-    unsigned int VBO, VAO /*, EBO*/; // vertex buffer object, vertex array
+    unsigned int VBO[2], VAO[2]; /*, EBO*/; // vertex buffer object, vertex array
                                      // object, element buffer object
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
+    glGenBuffers(2, VBO);
+    glGenVertexArrays(2, VAO);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(VAO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
                           (void *)0);
@@ -185,6 +182,15 @@ int main(int argc, char *argv[]) {
                           (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    glBindVertexArray(VAO[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(floor), floor, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe
     // glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
 
@@ -213,30 +219,45 @@ int main(int argc, char *argv[]) {
             glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float) width / (float) height, 0.1f, 100.0f);
             shader_program.set_mat4("projection", projection);
 
+            // glm::mat4 view = glm::lookAt(camera.position, glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f));
             glm::mat4 view = camera.get_view_matrix();
             shader_program.set_mat4("view", view);
 
-            glBindVertexArray(VAO);
-            float mod = 0.f;
-            for (unsigned int i = 0; i < 10; i++)
-            {
-                glm::mat4 model = glm::mat4(1.0f);
-                glm::vec3 rot_vec;
+            glm::mat4 model = glm::mat4(1.0f);
+            shader_program.set_mat4("model", model);
 
+            glBindVertexArray(VAO[1]);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+
+            glBindVertexArray(VAO[0]);
+            for (unsigned int i = 0; i < 10; i++) {
+                model = glm::mat4(1.0f);
                 model = glm::translate(model, cubePositions[i]);
-                if(i % 3 == 0 && i != 3) {
-                    mod = (float) glfwGetTime();
-                    if(i == 6 || i == 9)
-                        mod *= -1;
 
-                    rot_vec = glm::vec3(rand(), rand(), rand());
+                float angle = 20.f * i;
+                if(track) {
+                    std::cout << "\r"
+                        << " x: " << camera.position.x << std::setprecision(4) << std::fixed 
+                        << " y: " << camera.position.y << std::setprecision(4) << std::fixed
+                        << " z: " << camera.position.z << std::setprecision(4) << std::fixed << std::flush;
+
+                    glm::vec3 direction = glm::normalize(camera.position - cubePositions[i]);
+
+                    glm::vec3 right = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), direction));
+                    glm::vec3 up = glm::cross(direction, right);
+
+                    glm::mat4 rotationMatrix = glm::mat4(
+                        glm::vec4(right, 0.0f),
+                        glm::vec4(up, 0.0f),
+                        glm::vec4(-direction, 0.0f),
+                        glm::vec4(cubePositions[i], 1.0f)
+                    );
+
+                    model = rotationMatrix;
                 }
-                else {
-                    mod = 20.0f * i;
-                    rot_vec = glm::vec3(1.0f, 0.3f, 0.5f);
-                }
-                // float angle = 20.f * i;
-                model = glm::rotate(model, mod, rot_vec);
+                else
+                    model = glm::rotate(model, angle, glm::vec3(0.f, 1.f, 0.f));
+
                 shader_program.set_mat4("model", model);
 
                 glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -247,8 +268,8 @@ int main(int argc, char *argv[]) {
     }
 
     // cleanup
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(2, VAO);
+    glDeleteBuffers(2, VBO);
     glDeleteProgram(shader_program.program_ID);
     glfwDestroyWindow(window);
 
@@ -260,6 +281,14 @@ void process_input(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera.sprint(true);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
+        camera.sprint(false);
+
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
+        track = !track;
+
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.handle_keyboard(FORWARD, delta_time);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
@@ -269,10 +298,16 @@ void process_input(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.handle_keyboard(RIGHT, delta_time);
 
+
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS/*  || glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS */)
+        camera.crouch(true);
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_RELEASE /* || glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_RELEASE */)
+        camera.crouch(false);
+
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-        camera.jump_up();
+        camera.jump(true);
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE)
-        camera.jump_down();
+        camera.jump(false);
 }
 
 
