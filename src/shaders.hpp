2051f76ccd1da69146b67c77461cd5302e826776
jbrilha <j.brilha@campus.fct.unl.pt>
2024-12-29 20:13:00 +0000

    refactor: shaders

---
+++ src/shaders.hpp
@@ -0,0 +1,50 @@
+#ifndef SHADERS_H
+#define SHADERS_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+class Shader {
+  public:
+    unsigned int program_ID;
+
+    Shader(const char *vert_path, const char *frag_path) ;
+
+    void use() ;
+
+    void del() ;
+
+    void set_bool(const std::string &name, bool value) const ;
+
+    void set_int(const std::string &name, int value) const ;
+
+    void set_float(const std::string &name, float value) const ;
+
+    void set_vec2(const std::string &name, float x, float y) const ;
+
+    void set_vec2(const std::string &name, glm::vec2 &value) const ;
+
+    void set_vec3(const std::string &name, float x, float y, float z) const ;
+
+    void set_vec3(const std::string &name, glm::vec3 &value) const ;
+
+    void set_vec4(const std::string &name, float x, float y, float z,
+                  float w) const ;
+
+    void set_vec4(const std::string &name, glm::vec4 &value) const ;
+
+    void set_mat2(const std::string &name, glm::mat2 &mat) const ;
+
+    void set_mat3(const std::string &name, glm::mat3 &mat) const ;
+
+    void set_mat4(const std::string &name, glm::mat4 &mat) const ;
+
+  private:
+    void check_for_errors(unsigned int ID, std::string type) ;
+};
+
+#endif // !SHADERS_H
