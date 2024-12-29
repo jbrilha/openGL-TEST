484dffb869400ecf5426337e350bc4b72b189f15
jbrilha <j.brilha@campus.fct.unl.pt>
2024-12-29 20:10:48 +0000

    feat: different shaders for floor and cubes

---
+++ shaders/floor_vert.glsl
@@ -0,0 +1,20 @@
+#version 410 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec4 model_c0;
+layout (location = 3) in vec4 model_c1;
+layout (location = 4) in vec4 model_c2;
+layout (location = 5) in vec4 model_c3;
+
+out vec3 color;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+void main()
+{
+    mat4 model = mat4(model_c0, model_c1, model_c2, model_c3);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    color = aColor;
+}
